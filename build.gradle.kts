d8f892e8f99115cc12dc79aaaa69ea5ce0407d0c
Jacob <jacob.du-ad@hotmail.com>
2023-09-18 17:18:06 +0800

    init: Init Commit

---
+++ build.gradle.kts
@@ -0,0 +1,14 @@
+plugins {
+    kotlin("jvm") version "1.8.22"
+}
+
+group = "cn.soldat"
+version = "0.0.1"
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+    implementation(kotlin("stdlib"))
+}
---
+++ settings.gradle.kts
@@ -0,0 +1,2 @@
+rootProject.name = "jacob-utils"
+
